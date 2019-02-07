bc81dd4e2e74d67787baa23d9ea98b797f33d275
parth7166 <46794689+parth7166@users.noreply.github.com>
2019-02-07 14:18:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019WCSD3354WEEK05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new TestQuestion2();
+            a.PlayingWithForLoops();
+        }
+    }
+
+    class TestQuestion2
+    {
+        public int myFavoriteVariable = 0;
+
+        public void PlayingWithForLoops()
+        {
+            // write a For Loop to Add 10 Numbers
+            for (; MyMethod();)
+            {
+                if (myFavoriteVariable > 10)
+                {
+                    Console.WriteLine("i am so out of here!");
+                    break;
+                }
+                Console.WriteLine("oh no I have to go through this stupid loop again...");
+            }
+        }
+        public bool MyMethod()
+        {
+            myFavoriteVariable++;
+            return true;
+        }
+    }
+}
+
